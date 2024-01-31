408851d427947e570f6387b020e57a51b04bd025
BogdanNecatu <necatubogdan@gmail.com>
2024-01-31 18:11:24 +0100

    new feat

--- frontend/src/pages/Logout.js
+++ frontend/src/pages/Logout.js
@@ -1,16 +1,24 @@
 import { redirect } from 'react-router-dom';
 import axios from 'axios';
+import jwt_decode from 'jwt-decode';
 
-export function action({ id }) {
-  (async () => {
-    try {
-      const userIsLive = { connectedLive: false };
-      const res = await axios.patch('http://localhost:4000/api/users/' + id, userIsLive);
-      console.log(res);
-    } catch (error) {
-      console.log(error);
-    }
-  })();
+export function action() {
+  const token = localStorage.getItem('token');
+
+  if (token) {
+    const userDecode = jwt_decode(token);
+    const id = userDecode.id;
+
+    (async () => {
+      try {
+        const userIsLive = { connectedLive: false };
+        const res = await axios.patch('http://localhost:4000/api/users/' + id, userIsLive);
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }
 
   localStorage.removeItem('token');
   localStorage.removeItem('expiration');
